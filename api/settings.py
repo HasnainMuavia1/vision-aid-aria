b10db33bbe5cd896e9ff46e19045103d968dd3d7
Hasnain Muavia <hasnain.muavia2004@gmail.com>
2024-11-04 19:37:59 +0500

    interface variable commit

--- api/settings.py
+++ api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = "django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^"
 print("secret key",SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
